1cc8809a1dc3e6f63523713860dabb86e5bfcea4
Igor Roncevic <ironcev@hotmail.com>
2012-12-05 06:45:24 +0100

    AssemblyInfo of the HumblePractices.Tests.Unit project properly written.

--- Projects/HumblePractices.Tests.Unit/Properties/AssemblyInfo.cs
+++ Projects/HumblePractices.Tests.Unit/Properties/AssemblyInfo.cs
@@ -1,37 +1,16 @@
-﻿// TODO-IG: Fix all AssemblyInfo files in all projects. Add common assembly info.
-using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("HumblePractices.Tests.Unit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Humble Practices Unit Tests")]
+[assembly: AssemblyDescription("Unit tests for Humble Practices.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HumblePractices.Tests.Unit")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("The Humble Programmer")]
+[assembly: AssemblyProduct("Humble Practices")]
+[assembly: AssemblyCopyright("Copyright © 2012 Igor Roncevic")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5398e3e0-da9b-41e7-a9e2-920171d5bd64")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
