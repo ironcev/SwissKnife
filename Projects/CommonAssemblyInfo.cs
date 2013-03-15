ae1f61a569a8ff8b23da5fa4af0d8bf53937671e
Igor Roncevic <ironcev@hotmail.com>
2013-03-15 07:56:56 +0100

    CommonAssemblyInfo added.

---
+++ Projects/CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Humble Practices")]
+[assembly: AssemblyConfiguration("")] // TODO-IG: Define proper assembly configuration.
+[assembly: AssemblyCompany("The Humble Programmer")]
+[assembly: AssemblyProduct("Humble Practices")]
+[assembly: AssemblyCopyright("Copyright © 2012 Igor Roncevic")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")] // TODO-IG: Think about assembly culture.
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyInformationalVersion("0.1.*")]
--- Projects/HumblePractices.Tests.Unit/Properties/AssemblyInfo.cs
+++ Projects/HumblePractices.Tests.Unit/Properties/AssemblyInfo.cs
@@ -1,16 +1,3 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Humble Practices Unit Tests")]
 [assembly: AssemblyDescription("Unit tests for Humble Practices.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The Humble Programmer")]
-[assembly: AssemblyProduct("Humble Practices")]
-[assembly: AssemblyCopyright("Copyright © 2012 Igor Roncevic")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
