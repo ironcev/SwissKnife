0080f5c84e6facc055357b53e7f86538cd3b92a0
Igor Roncevic <ironcev@hotmail.com>
2014-03-06 18:03:48 +0100

    Fixed indentation in the ArgumentTests class. Assembly info removed from the Unit Test project.

--- Source/SwissKnife.Tests.Unit/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// ReSharper disable CheckNamespace
-internal static partial class AssemblyDescription
-{
-    public const string Title = Product + " Unit Tests";
-    public const string Description = "Unit tests for " + Product + " core abstractions.";
-}
-// ReSharper restore CheckNamespace
