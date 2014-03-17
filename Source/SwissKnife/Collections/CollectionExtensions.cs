f674acab7169998a4b0f6449afe2a3588a3d5a93
Igor Roncevic <ironcev@hotmail.com>
2014-03-17 19:09:28 +0100

    Improved comments on CollectionExtensions class members.

--- Source/SwissKnife/Collections/CollectionExtensions.cs
+++ Source/SwissKnife/Collections/CollectionExtensions.cs
@@ -12,9 +12,10 @@ namespace SwissKnife.Collections
         /// <summary>
         /// Executes the <paramref name="action"/> on each element of the <paramref name="enumerable"/>.
         /// </summary>
+        /// <typeparam name="T">The type of objects to enumerate.</typeparam>
         /// <param name="enumerable">The enumerable to execute the <paramref name="action"/> on.</param>
         /// <param name="action">The action to execute on the each element of the <paramref name="enumerable"/>.</param>
-        /// <exception cref="ArgumentNullException">If <paramref name="enumerable"/> or <paramref name="action"/> are null.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="enumerable"/> is null.<br/>-or-<br/><paramref name="action"/> is null.</exception>
         public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
         {
             #region Preconditions
@@ -32,8 +33,8 @@ namespace SwissKnife.Collections
         /// <typeparam name="T">The type of the items in the <paramref name="collection"/>.</typeparam>
         /// <param name="collection">The <see cref="ICollection{T}"/> to which to add <paramref name="itemsToAdd"/>.</param>
         /// <param name="itemsToAdd">Items that has to be added to the <paramref name="collection"/>.</param>
-        /// <exception cref="ArgumentNullException">If <paramref name="collection"/> or <paramref name="itemsToAdd"/> are null.</exception>
-        /// <exception cref="NotSupportedException">If <paramref name="collection"/> is read-only.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="collection"/> is null.<br/>-or-<br/><paramref name="itemsToAdd"/> is null.</exception>
+        /// <exception cref="NotSupportedException"><paramref name="collection"/> is read-only.</exception>
         public static void AddMany<T>(this ICollection<T> collection, IEnumerable<T> itemsToAdd)
         {
             #region Preconditions
@@ -46,19 +47,27 @@ namespace SwissKnife.Collections
         }
 
         /// <summary>
-        /// Gets the value from the <paramref name="dictionary"/> associated with the specified <paramref name="key"/>.
-        /// If the <paramref name="dictionary"/> does not contain the <paramref name="key"/>, a new <see cref="KeyValuePair{TKey,TValue}"/>
-        /// will be added to the <paramref name="dictionary"/>.
-        /// The key and the value of that new <see cref="KeyValuePair{TKey,TValue}"/> will be set to <paramref name="key"/> and the result of the <paramref name="getValueToAdd"/> respectively.
+        /// Gets the value from the <paramref name="dictionary"/> associated with the specified <paramref name="key"/>. If the value does not exist in the <paramref name="dictionary"/> it will be added to it.
         /// </summary>
+        /// <remarks>
+        /// If the <paramref name="dictionary"/> does not contain the <paramref name="key"/>, a new <see cref="KeyValuePair{TKey,TValue}"/> will be added to the <paramref name="dictionary"/>.
+        /// The key and the value of that new <see cref="KeyValuePair{TKey,TValue}"/> will be set to <paramref name="key"/> and the result of the <paramref name="getValueToAdd"/> respectively.
+        /// The <paramref name="getValueToAdd"/> is called only if the <paramref name="dictionary"/> does not already contain the <paramref name="key"/>.
+        /// <br/>
+        /// <b>Note</b>
+        /// <br/>
+        /// If the <paramref name="getValueToAdd"/> throws any exception, that exception will be propagated to the caller.
+        /// </remarks>
+        /// <typeparam name="TKey">The type of keys in the <paramref name="dictionary"/>.</typeparam>
+        /// <typeparam name="TValue">The type of values in the <paramref name="dictionary"/>.</typeparam>
         /// <param name="dictionary">The <see cref="IDictionary{TKey,TValue}"/> from which the value has to be get.</param>
         /// <param name="key">The key associated with the value.</param>
         /// <param name="getValueToAdd">
         /// The function that returns the value that has to be inserted into the <paramref name="dictionary"/> if it does not contain the <paramref name="key"/>.
-        /// This function will be called only if the <paramref name="dictionary"/> does not contain the <paramref name="key"/>.
+        /// This function will be called only if the <paramref name="dictionary"/> does not already contain the <paramref name="key"/>.
         /// </param>
-        /// <exception cref="ArgumentNullException">If <paramref name="dictionary"/> or <paramref name="key"/> or <paramref name="getValueToAdd"/> are null.</exception>
-        /// <exception cref="NotSupportedException">If <paramref name="dictionary"/> is read-only.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="dictionary"/> is null.<br/>-or-<br/><paramref name="key"/> is null.<br/>-or-<br/><paramref name="getValueToAdd"/> is null.</exception>
+        /// <exception cref="NotSupportedException"><paramref name="dictionary"/> is read-only.</exception>
         public static TValue GetValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> getValueToAdd)
         {
             #region Preconditions
