fba304982501c1583e14f4f4b3d28316d813162f
Igor Roncevic <ironcev@hotmail.com>
2014-04-03 07:27:23 +0200

    StringConvert.ToInt64() added.

--- Source/SwissKnife.Tests.Unit/StringConvertTests.cs
+++ Source/SwissKnife.Tests.Unit/StringConvertTests.cs
@@ -200,6 +200,200 @@ namespace SwissKnife.Tests.Unit
             Assert.That(StringConvert.ToInt32Or(12345.ToString(CultureInfo.InvariantCulture), 123), Is.EqualTo(12345));
         }
         #endregion
+
+        #region ToInt64
+        [Test]
+        public void ToInt64_ValueIsNone_ReturnsNull()
+        {
+            Assert.That(StringConvert.ToInt64(null).HasValue, Is.False);
+        }
+
+        [Test]
+        public void ToInt64_ValueIsEmpty_ReturnsNull()
+        {
+            Assert.That(StringConvert.ToInt64(string.Empty).HasValue, Is.False);
+        }
+
+        [Test]
+        public void ToInt64_ValueIsWhiteSpace_ReturnsNull()
+        {
+            Assert.That(StringConvert.ToInt64(" ").HasValue, Is.False);
+        }
+
+        [Test]
+        public void ToInt64_ValueIsNotAnInt64_ReturnsNull()
+        {
+            Assert.That(StringConvert.ToInt64("QWERT").HasValue, Is.False);
+        }
+
+        [Test]
+        public void ToInt64_ValueIsLessThanMinInt64_ReturnsNull()
+        {
+            Assert.That(StringConvert.ToInt64(long.MinValue + "0").HasValue, Is.False);
+        }
+
+        [Test]
+        public void ToInt64_ValueIsGreaterThanMaxInt64_ReturnsNull()
+        {
+            Assert.That(StringConvert.ToInt64(long.MaxValue + "0").HasValue, Is.False);
+        }
+
+        [Test]
+        public void ToInt64_ValueIsMinInt64_ReturnsMinInteger()
+        {
+            // ReSharper disable PossibleInvalidOperationException
+            Assert.That(StringConvert.ToInt64(long.MinValue.ToString(CultureInfo.InvariantCulture)).Value, Is.EqualTo(long.MinValue));
+            // ReSharper restore PossibleInvalidOperationException
+        }
+
+        [Test]
+        public void ToInt64_ValueIsMaxInt64_ReturnsMaxInteger()
+        {
+            // ReSharper disable PossibleInvalidOperationException
+            Assert.That(StringConvert.ToInt64(long.MaxValue.ToString(CultureInfo.InvariantCulture)).Value, Is.EqualTo(long.MaxValue));
+            // ReSharper restore PossibleInvalidOperationException
+        }
+
+        [Test]
+        public void ToInt64_ValueIsZero_ReturnsZero()
+        {
+            // ReSharper disable PossibleInvalidOperationException
+            Assert.That(StringConvert.ToInt64(0.ToString(CultureInfo.InvariantCulture)).Value, Is.EqualTo(0));
+            // ReSharper restore PossibleInvalidOperationException
+        }
+
+        [Test]
+        public void ToInt64_ValueIsArbitraryInt64_ReturnsThatInt64()
+        {
+            // ReSharper disable PossibleInvalidOperationException
+            Assert.That(StringConvert.ToInt64(123.ToString(CultureInfo.InvariantCulture)).Value, Is.EqualTo(123));
+            // ReSharper restore PossibleInvalidOperationException
+        }
+        #endregion
+
+        #region ToInt64OrZero
+        [Test]
+        public void ToInt64OrZero_ValueIsNone_ReturnsZero()
+        {
+            Assert.That(StringConvert.ToInt64OrZero(null), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ToInt64OrZero_ValueIsEmpty_ReturnsZero()
+        {
+            Assert.That(StringConvert.ToInt64OrZero(string.Empty), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ToInt64OrZero_ValueIsWhiteSpace_ReturnsZero()
+        {
+            Assert.That(StringConvert.ToInt64OrZero(" "), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ToInt64OrZero_ValueIsNotAnInt64_ReturnsZero()
+        {
+            Assert.That(StringConvert.ToInt64OrZero("QWERT"), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ToInt64OrZero_ValueIsLessThanMinInt64_ReturnsZero()
+        {
+            Assert.That(StringConvert.ToInt64OrZero(long.MinValue + "0"), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ToInt64OrZero_ValueIsGreaterThanMaxInt64_ReturnsZero()
+        {
+            Assert.That(StringConvert.ToInt64OrZero(long.MaxValue + "0"), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ToInt64OrZero_ValueIsMinInt64_ReturnsMinInteger()
+        {
+            Assert.That(StringConvert.ToInt64OrZero(long.MinValue.ToString(CultureInfo.InvariantCulture)), Is.EqualTo(long.MinValue));
+        }
+
+        [Test]
+        public void ToInt64OrZero_ValueIsMaxInt64_ReturnsMaxInteger()
+        {
+            Assert.That(StringConvert.ToInt64OrZero(long.MaxValue.ToString(CultureInfo.InvariantCulture)), Is.EqualTo(long.MaxValue));
+        }
+
+        [Test]
+        public void ToInt64OrZero_ValueIsZero_ReturnsZero()
+        {
+            Assert.That(StringConvert.ToInt64OrZero(0.ToString(CultureInfo.InvariantCulture)), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ToInt64OrZero_ValueIsArbitraryInt64_ReturnsThatInt64()
+        {
+            Assert.That(StringConvert.ToInt64OrZero(123.ToString(CultureInfo.InvariantCulture)), Is.EqualTo(123));
+        }
+        #endregion
+
+        #region ToInt64Or
+        [Test]
+        public void ToInt64Or_ValueIsNone_ReturnsDefaultValue()
+        {
+            Assert.That(StringConvert.ToInt64Or(null, 123), Is.EqualTo(123));
+        }
+
+        [Test]
+        public void ToInt64Or_ValueIsEmpty_ReturnsDefaultValue()
+        {
+            Assert.That(StringConvert.ToInt64Or(string.Empty, 123), Is.EqualTo(123));
+        }
+
+        [Test]
+        public void ToInt64Or_ValueIsWhiteSpace_ReturnsDefaultValue()
+        {
+            Assert.That(StringConvert.ToInt64Or(" ", 123), Is.EqualTo(123));
+        }
+
+        [Test]
+        public void ToInt64Or_ValueIsNotAnInt64_ReturnsDefaultValue()
+        {
+            Assert.That(StringConvert.ToInt64Or("QWERT", 123), Is.EqualTo(123));
+        }
+
+        [Test]
+        public void ToInt64Or_ValueIsLessThanMinInt64_ReturnsDefaultValue()
+        {
+            Assert.That(StringConvert.ToInt64Or(long.MinValue + "0", 123), Is.EqualTo(123));
+        }
+
+        [Test]
+        public void ToInt64Or_ValueIsGreaterThanMaxInt64_ReturnsDefaultValue()
+        {
+            Assert.That(StringConvert.ToInt64Or(long.MaxValue + "0", 123), Is.EqualTo(123));
+        }
+
+        [Test]
+        public void ToInt64Or_ValueIsMinInt64_ReturnsMinInteger()
+        {
+            Assert.That(StringConvert.ToInt64Or(long.MinValue.ToString(CultureInfo.InvariantCulture), 123), Is.EqualTo(long.MinValue));
+        }
+
+        [Test]
+        public void ToInt64DefaultValue_ValueIsMaxInt64_ReturnsMaxInteger()
+        {
+            Assert.That(StringConvert.ToInt64Or(long.MaxValue.ToString(CultureInfo.InvariantCulture), 123), Is.EqualTo(long.MaxValue));
+        }
+
+        [Test]
+        public void ToInt64Or_ValueIsZero_ReturnsZero()
+        {
+            Assert.That(StringConvert.ToInt64Or(0.ToString(CultureInfo.InvariantCulture), 123), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ToInt64Or_ValueIsArbitraryInt64_ReturnsThatInt64()
+        {
+            Assert.That(StringConvert.ToInt64Or(12345.ToString(CultureInfo.InvariantCulture), 123), Is.EqualTo(12345));
+        }
+        #endregion
     }
     // ReSharper restore InconsistentNaming
 }
--- Source/SwissKnife/StringConvert.cs
+++ Source/SwissKnife/StringConvert.cs
@@ -24,7 +24,7 @@
         }
 
         /// <summary>
-        /// Converts the string representation of a number to its 32-bit signed integer equivalent.
+        /// Converts the string representation of a number to its 32-bit signed integer equivalent or specified default value if the conversion does not succeed.
         /// </summary>
         /// <param name="value">A <see cref="string"/> containing a number to convert.</param>
         /// <param name="defaultValue">Default value to return if the conversion fails.</param>
@@ -42,7 +42,7 @@
         }
 
         /// <summary>
-        /// Converts the string representation of a number to its 32-bit signed integer equivalent.
+        /// Converts the string representation of a number to its 32-bit signed integer equivalent or zero if the conversion does not succeed.
         /// </summary>
         /// <param name="value">A <see cref="string"/> containing a number to convert.</param>
         /// <returns>
@@ -53,6 +53,55 @@
         public static int ToInt32OrZero(Option<string> value)
         {
             return ToInt32Or(value, 0);
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number to its 64-bit signed integer equivalent. The return value indicates whether the conversion succeeded.
+        /// </summary>
+        /// <param name="value">A <see cref="string"/> containing a number to convert.</param>
+        /// <returns>
+        /// 64-bit signed integer value equivalent to the number contained in the <paramref name="value"/> if the conversion succeeded.
+        /// <br/>-or-<br/>Null if the <paramref name="value"/> is None option.
+        /// <br/>-or-<br/>Null if the conversion failed.
+        /// </returns>
+        public static long? ToInt64(Option<string> value)
+        {
+            // The TryParse() fails if the string parameter is null, is not of the correct format, or represents a number less than long.MinValue or greater than long.MaxValue.
+            // Long story short, we don't need additional check if the Option is None.
+            long result;
+            return long.TryParse(value.ValueOrNull, out result) ? result : (long?)null;
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number to its 64-bit signed integer equivalent or specified default value if the conversion does not succeed.
+        /// </summary>
+        /// <param name="value">A <see cref="string"/> containing a number to convert.</param>
+        /// <param name="defaultValue">Default value to return if the conversion fails.</param>
+        /// <returns>
+        /// 64-bit signed integer value equivalent to the number contained in the <paramref name="value"/> if the conversion succeeded.
+        /// <br/>-or-<br/><paramref name="defaultValue"/> if the <paramref name="value"/> is None option.
+        /// <br/>-or-<br/><paramref name="defaultValue"/> if the conversion failed.
+        /// </returns>
+        public static long ToInt64Or(Option<string> value, long defaultValue)
+        {
+            // The TryParse() fails if the string parameter is null, is not of the correct format, or represents a number less than long.MinValue or greater than long.MaxValue.
+            // Long story short, we don't need additional check if the Option is None.
+            long result;
+            return long.TryParse(value.ValueOrNull, out result) ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number to its 64-bit signed integer equivalent or zero if the conversion does not succeed.
+        /// </summary>
+        /// <param name="value">A <see cref="string"/> containing a number to convert.</param>
+        /// <returns>
+        /// 64-bit signed integer value equivalent to the number contained in the <paramref name="value"/> if the conversion succeeded.
+        /// <br/>-or-<br/>Zero if the <paramref name="value"/> is None option.
+        /// <br/>-or-<br/>Zero if the conversion failed.
+        /// </returns>
+        public static long ToInt64OrZero(Option<string> value)
+        {
+            return ToInt64Or(value, 0);
         }
     }
 }
