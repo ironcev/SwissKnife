5249adca221d7f919bd680931d7438d68c01a6ca
Igor Roncevic <ironcev@hotmail.com>
2014-04-05 20:46:16 +0200

    SwissKnife.Windows empty project added and its NuGet package created.

---
+++ Source/SwissKnife.Windows/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// ReSharper disable CheckNamespace
+internal static partial class AssemblyDescription
+{
+    public const string Title = Product + " for .NET Windows programming";
+    public const string Description = Product + " for .NET Windows programming.";
+}
+// ReSharper restore CheckNamespace
