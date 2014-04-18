3b55d71b65a94406818e205036bcf5d1fb05469c
Igor Roncevic <ironcev@hotmail.com>
2014-04-18 08:20:55 +0200

    NamespaceDoc classes added for all namespaces in the SwissKnife.Web project.

---
+++ Source/SwissKnife.Web/Mvc/NamespaceDoc.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SwissKnife.Web.Mvc
+{
+    /// <summary>
+    /// The SwissKnife.Web.Mvc namespace contains extensions for classes that can be found in the <see cref="System.Web.Mvc"/> namespace.
+    /// </summary>
+    [CompilerGenerated]
+    class NamespaceDoc {}
+}
---
+++ Source/SwissKnife.Web/Routing/NamespaceDoc.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SwissKnife.Web.Routing
+{
+    /// <summary>
+    /// The SwissKnife.Web.Routing namespace contains classes for easier manipulation of <see cref="System.Web"/> routes.
+    /// </summary>
+    [CompilerGenerated]
+    class NamespaceDoc {}
+}
